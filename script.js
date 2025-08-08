a0e91266b8609a3b99d37dfcaf41172cb0b2d1c6
Miya Kuo <miya3927@gmail.com>
2025-08-07 20:48:14 -0700

    Finished exerecise

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
-$(function() {
+$(function () {
   // your code here
+  $('.accordion-header').on('click', function () {
+    $('.accordion-header').not(this).next('.accordion-content').slideUp()
+    $(this).next('.accordion-content').slideToggle()
+  })
+
+  $('.todos button').on('click', function () {
+    $.ajax({
+      url: "https://dummyjson.com/todos",
+      type: "GET",
+      success: function (res) {
+        const todos = res.todos
+        buildTodos(todos)
+      },
+      error: function (err) {
+        console.error(err)
+      }
+    })
+  })
+
+  function buildTodos(arr) {
+
+    const list = $('.todos button').next('ul')
+    list.empty()
+    arr.forEach(el => {
+      const li = $('<li></li>')
+      li.text(el.todo)
+      list.append(li)
+    });
+  }
 })
